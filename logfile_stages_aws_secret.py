be415c0e8bd9d1f49152fb34b30f66e6391f102c
cyberopsgit <cyberopsgit@cyberopstrainings.com>
2026-01-12 20:53:06 +0530

    Refactor get_secret function to remove unused code
    Removed unused session and client creation for AWS Secrets Manager.

--- logfile_stages_aws_secret.py
+++ logfile_stages_aws_secret.py
@@ -28,13 +28,6 @@ from botocore.exceptions import ClientError
 def get_secret():
     secret_name = "qualys-gs-dev-f-paloalt-92806b-qualys-secret"
     region_name = "us-east-1"
-
-    session = boto3.session.Session()
-    client = session.client(
-        service_name="secretsmanager",
-        region_name=region_name
-    )
-
     try:
         response = client.get_secret_value(SecretId=secret_name)
     except ClientError as e:
