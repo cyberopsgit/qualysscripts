7cee669caf27574af272a9ebbf5b4e374e5e40b6
cyberopsgit <cyberopsgit@cyberopstrainings.com>
2026-01-12 22:21:57 +0530

    Improve logging for AWS Secrets Manager error

--- logfile_stages_aws_secret.py
+++ logfile_stages_aws_secret.py
@@ -37,8 +37,8 @@ def get_secret():
     try:
         response = client.get_secret_value(SecretId=secret_name)
     except ClientError as e:
-        logger.critical("Failed to fetch secret from AWS Secrets Manager")
-        raise e
+        logger.critical("Failed to fetch secret from AWS Secrets Manager:", e)
+        raise
 
     return json.loads(response["SecretString"])
 
